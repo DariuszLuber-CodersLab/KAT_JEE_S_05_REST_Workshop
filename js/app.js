656d1719c11386c19928335a827599d34b51051f
Dariusz Luber <dariusz.luber@gmail.com>
2019-01-11 13:59:27 +0100

    show books

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-document.addEventListener(function(){
-    console.log("test branches");
+document.addEventListener("DOMContentLoaded",function(){
+
+    getBooks();
+
 })
+
+
+function getBooks(){
+    $.ajax({
+        url: "http://localhost:8282/books",
+        dataType: "json"
+    })
+    .done(function(books){
+        let listEl = document.getElementById("bookList");
+        books.forEach( book => addBookToList(listEl, book) );
+    })
+}
+
+function addBookToList(listEl ,bookObj){
+    let newLi = document.createElement("li");
+    let h2El = document.createElement("h2");
+    h2El.innerText = bookObj.title;
+
+    newLi.appendChild(h2El);
+    listEl.appendChild(newLi)
+}
+
+
